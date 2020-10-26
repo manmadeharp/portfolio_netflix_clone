9737396e203dd985c9590f8918eae864090eca16
Finbar <finbarccowan@gmail.com>
2020-10-26 10:47:34 +0000

    commit

--- src/components/Row.js
+++ src/components/Row.js
@@ -3,7 +3,7 @@ import axios from '../axios'
 import "./Row.css"
 const base_url = "https://image.tmdb.org/t/p/original"
 
-function Row({title, fetchUrl}) {
+function Row({title, fetchUrl, isLargeRow}) {
     const [movies, setMovies] = useState([]);
 
     useEffect(() => {
@@ -14,16 +14,20 @@ function Row({title, fetchUrl}) {
         }
         fetchData();
     }, [fetchUrl])
-    console.table(movies)
-    return (
+        return (
         <div className="row">
             <h2>{title}</h2>
             
             <div className="row_posters">
                 {movies.map(movie => (
-                    <img className="row_poster"
-                    src={`${base_url}${movie.poster_path}`} 
-                    alt={movie.name}></img>
+                   <img 
+                    key={movie.id}
+                    className={`row_poster ${isLargeRow && "row_posterLarge"}` }
+                    src={`${base_url}${isLargeRow ? movie.poster_path: movie.backdrop_path}`} 
+                    alt={movie.name}
+                    />
+
+                    
                 ))}
             </div>
 
